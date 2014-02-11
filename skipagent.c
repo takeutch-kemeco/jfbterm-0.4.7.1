77b50ed4f3a74fd3d5375690aba54e86ce9ab1a1
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-02-12 05:02:35 +0900

    refactorint: skipagent のプログラムフローを簡単な形に修正
    フラグの数を削減して、ループをモジュール分けして代替表現することで、少し読みやすくした。
    また、処理速度重視でmutexを用いずに行っていた処理を、mutexを用いるようい修正。（結果として従来よりも遅くなる）
    前の C -> Haskell コンバートが失敗した原因の一つとして、プログラムフローがわけ分からなかったのも関係していたが、
    今回のリファクタリングで相当に見通し良く改善されたので、これをベースに再び C -> Haskell を試してみれば、
    もしかしたら今度は成功するかもしれない。

--- skipagent.c
+++ skipagent.c
@@ -1,9 +1,33 @@
-/* skipagent.c 2012 Takeutch Kemeco
- * 3-clause BSD lisence
+/*
+ * skipagent.c
+ * Copyright (C) 2012, 2014 Takeutch Kemeco
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORITOSHI MASUICHI ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL NORITOSHI MASUICHI BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <pthread.h>
 #include <time.h>
@@ -15,8 +39,7 @@ bool sage_use = true;
 
 static pthread_t thread;
 
-static const unsigned long long limit_time = (1000 * 1000) * 16;
-static const unsigned long long bound_time = (1000 * 1000) * 8;
+static const uint64_t bound_time = (1000 * 1000) * 16;
 
 #define LOOP_TIME ((1000 * 1000) * 4)
 static const struct timespec loop_time_ts = {
@@ -24,114 +47,109 @@ static const struct timespec loop_time_ts = {
 	.tv_nsec = LOOP_TIME,
 };
 
-#define REFRESH_TIME ((1000 * 1000) * 8)
-static const struct timespec refresh_time_ts = {
-	.tv_sec  = 0,
-	.tv_nsec = REFRESH_TIME,
-};
-
-static volatile unsigned long long start_time = 0;
-static volatile unsigned long long cur_time   = 0;
-static volatile unsigned long long prev_time  = 0;
+static volatile uint64_t cur_time = 0;
+static volatile uint64_t prev_time = 0;
 
 static volatile bool run_flag = false;
 static volatile bool order_flag = false;
-static volatile bool refresh_flag = false;
 
 static sage_throw_func cur_func = NULL;
 static void* cur_param = NULL;
 
-static unsigned long long rdtsc(void)
+static int mutex_tmp = 0;
+
+static uint64_t rdtsc(void)
 {
-	volatile unsigned long long A;
-	__asm__ volatile("rdtsc;":"=r"(A));
-	return A;
+	volatile uint64_t RAX;
+	__asm__ volatile("rdtsc;":"=r"(RAX));
+	return RAX;
 }
 
-static bool is_skip(void)
+static void lock_mutex(const int id)
 {
-	if(cur_time - prev_time < bound_time) {
-		if(cur_time - start_time >= limit_time) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+        while (1) {
+                __sync_val_compare_and_swap(&mutex_tmp, 0, id);
+                if (mutex_tmp == id)
+                        return;
 
-	return false;
+                nanosleep(&loop_time_ts, NULL);
+        }
 }
 
+static void unlock_mutex(const int id)
+{
+        while (1) {
+                __sync_val_compare_and_swap(&mutex_tmp, id, 0);
+                if (mutex_tmp == 0)
+                        return;
+
+                nanosleep(&loop_time_ts, NULL);
+        }
+}
+
+static void peace_loop(void)
+{
+        while (run_flag && order_flag)
+                nanosleep(&loop_time_ts, NULL);
+}
+
+static void transient_loop(void)
+{
+        const int transient_loop_id = 1;
+
+        while (run_flag) {
+                cur_time = rdtsc();
+
+                if (order_flag) {
+                        if ((cur_time - prev_time) > bound_time) {
+                                lock_mutex(transient_loop_id);
+                                cur_func(cur_param);
+                                order_flag = false;
+                                unlock_mutex(transient_loop_id);
+
+                                peace_loop();
+
+                                prev_time = cur_time;
+                        }
+                }
+
+                nanosleep(&loop_time_ts, NULL);
+        }
+}
+ 
 static void* main_loop(void* _a)
 {
-	while(run_flag) {
-		if(order_flag) {
-			cur_time = rdtsc();
-			if(is_skip() == false) {
-				order_flag = false;
-				refresh_flag = true;
-			}
-
-			prev_time = cur_time;
-		}
-
-		if(refresh_flag) {
-			refresh_flag = false;
-
-			nanosleep(&refresh_time_ts, NULL);
-			cur_func(cur_param);
-		}
-
-		nanosleep(&loop_time_ts, NULL);
-	}
-
-	run_flag = true;
+        if (sage_use)
+                transient_loop();
 
 	return NULL;
 }
 
 void sage_throw(sage_throw_func func, void* param)
 {
-	switch(sage_use) {
-	case true:
-		if(run_flag) {
-			order_flag = true;
+        const int sage_throw_mutex_id = 2;
 
-			if(order_flag == true) {
-				cur_param = param;
-				cur_func = func;
-			}
-		}
-		break;
-
-	case false:
-		func(param);
-		break;
+        if (sage_use) {
+		lock_mutex(sage_throw_mutex_id);
+                cur_func = func;
+                cur_param = param;
+                order_flag = true;
+                unlock_mutex(sage_throw_mutex_id);
+	} else {
+                func(param);
 	}
 }
 
 void sage_init(void)
 {
-	start_time = 0;
-	cur_time = 0;
-	prev_time = 0;
-
-	order_flag = false;
 	run_flag = true;
 
-	cur_func = NULL;
-	cur_param = NULL;
-
-	if(pthread_create(&thread, NULL, main_loop, NULL) != 0) {
+	if (pthread_create(&thread, NULL, main_loop, NULL) != 0)
 		die("sage_init(): thread create err\n");
-	}
 }
-
-static void dummy_func(void* _a) {}
 
 void sage_close(void)
 {
 	run_flag = false;
-	cur_func = dummy_func;
-
 	pthread_join(thread, NULL);
 }
