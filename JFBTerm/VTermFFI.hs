de220d4dec8616141b12d667aae9cfbd44797630
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-03-10 02:44:07 +0900

    skelton add: JFBTerm/{VTerm,VTermFFI}.hs
    vterm.c -> VTerm{,FFI}.hs を行うためのスケルトンを追加。
    実際には何も動作しない。

---
+++ JFBTerm/VTermFFI.hs
@@ -0,0 +1,37 @@
+-- | JFBTerm/VTermFFI.hs
+-- | Copyright (C) 2014 Takeutch Kemeco (takeutchkemeco@gmail.com)
+-- |
+-- | JFBTERM -
+-- | Copyright (c) 2003  Fumitoshi UKAI <ukai@debian.or.jp>
+-- | Copyright (C) 1999  Noritoshi MASUICHI (nmasu@ma3.justnet.ne.jp)
+-- |
+-- | KON2 - Kanji ON Console -
+-- | Copyright (C) 1992-1996 Takashi MANABE (manabe@papilio.tutics.tut.ac.jp)
+-- |
+-- | Redistribution and use in source and binary forms, with or without
+-- | modification, are permitted provided that the following conditions
+-- | are met:
+-- | 1. Redistributions of source code must retain the above copyright
+-- |    notice, this list of conditions and the following disclaimer.
+-- | 2. Redistributions in binary form must reproduce the above copyright
+-- |    notice, this list of conditions and the following disclaimer in the
+-- |    documentation and/or other materials provided with the distribution.
+-- |
+-- | THIS SOFTWARE IS PROVIDED BY NORITOSHI MASUICHI ``AS IS'' AND ANY
+-- | EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- | IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-- | ARE DISCLAIMED.  IN NO EVENT SHALL THE TERRENCE R. LAMBERT BE LIABLE
+-- | FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- | DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+-- | OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+-- | HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+-- | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+-- | OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- | SUCH DAMAGE.
+
+module JFBTerm.VTermFFI where
+
+import JFBTerm.VTerm
+import Foreign.Storable (Storable(..))
+import Foreign.C.Types -- (CUInt)
+import Foreign.Ptr (Ptr, nullPtr)
